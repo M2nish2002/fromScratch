2357ca3577694d8404d6bbe2905dc62d62f41b39
manish <mroutjaysingh@gmail.com>
2025-06-22 20:31:57 +0530

    updated regression class

--- Regression/models/BaseRegressionModel.py
+++ Regression/models/BaseRegressionModel.py
@@ -7,17 +7,18 @@ class Regression():
     
     def weight_initia(self,n_features):
          limit=1/math.sqrt(n_features)
-         self.w=np.random.uniform(-limit,limit,(n_features,))
+         self.w=np.random.uniform(-limit,limit,(n_features,1))
     
     def fit(self,X,y):
          X=np.insert(X,0,1,axis=1)
+         y=y.reshape(-1,1)
          self.training_errors=[]
          self.weight_initia(n_features=X.shape[1])
          for i in range(1,self.n_iterations):
               y_pred=X.dot(self.w)
               mse=np.mean(0.5*(y-y_pred)**2)+self.regularization(self.w)
               self.training_errors.append(mse)
-              grad_w=-(y-y_pred).dot(X)+self.regularization.grad(self.w)
+              grad_w=(1/X.shape[0])*X.T.dot(y_pred-y)+self.regularization.grad(self.w)
               self.w-=self.learn_rate*grad_w
     
     def predict(self,X):
